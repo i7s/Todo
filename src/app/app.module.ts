e1935d7473cae77fbd163453f880e363a4cce518
i7s <aleksadmiy@gmail.com>
2024-07-24 10:25:34 +0300

    Added sort and pagination for table with Material Design components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {CategoriesComponent} from "./views/categories/categories.component";
 import {TasksComponent} from "./views/tasks/tasks.component";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import {TasksComponent} from "./views/tasks/tasks.component";
     TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,13 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from "../../model/Task";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
+
+  protected displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  protected dataSource!: MatTableDataSource<Task>;
+
+  @ViewChild(MatPaginator, {static: false}) private paginator!: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort!: MatSort;
 
   tasks: Task[] = [];
 
@@ -16,10 +25,55 @@ export class TasksComponent implements OnInit {
 
   ngOnInit() {
     this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
+  }
+
+  ngAfterViewInit(): void {
+    this.addTableObjects();
   }
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  protected getPriorityColor(task: Task) {
+    if (task.completed) {
+      return "#F8F9FA";
+    }
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+    return "#fff";
+  }
+
+  private refreshTable() {
+    this.dataSource.data = this.tasks;
+
+    this.addTableObjects();
+
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+        case 'title': {
+          return task.title;
+        }
+      }
+    }
+  }
+
+  private addTableObjects() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
